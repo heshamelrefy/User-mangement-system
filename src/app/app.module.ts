f4586af343f99ab903797694b59d6b7d88332a1f
Hisham Eid Aly <Hisham.Eid@andalusiagroup.net>
2024-10-28 12:54:07 +0200

    add service worker & update config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +13,8 @@ import { PagesModule } from './pages/pages.module';
 import { ErrorInterceptor } from './core/helpers/error.interceptor';
 import { AccountModule } from './account/account.module';
 import { ExtrapagesModule } from './extrapages/extrapages.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from './environment/enviroment-prod';
 
 export function createTranslateLoader(http:HttpClient):any
 {
@@ -39,7 +41,13 @@ export function createTranslateLoader(http:HttpClient):any
     PagesModule,
     AccountModule,
     HttpClientModule,
-    ExtrapagesModule
+    ExtrapagesModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{
     provide:HTTP_INTERCEPTORS,
